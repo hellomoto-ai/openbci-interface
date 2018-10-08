3cfe49a485b1ef275529153f8e159ef97bdccab2
moto <moto@hellomoto.ai>
2018-10-07 21:53:13 -0700

    Fix documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ import sys
 sys.path.insert(
     0,
     os.path.abspath(
-        os.path.join(os.path.dirname(__file__), '..', '..')
+        os.path.join(os.path.dirname(__file__), '..', '..', 'src')
     ),
 )
 
